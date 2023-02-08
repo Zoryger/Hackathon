ddeee1618bfd209089645cdf8e45c3cae1b5b42b
Thibault <112929050+Zoryger@users.noreply.github.com>
2023-02-09 00:54:57 +0100

    Update useWeather.tsx

--- src/hooks/useWeather.tsx
+++ src/hooks/useWeather.tsx
@@ -1,13 +1,12 @@
 import { useQuery } from "@tanstack/react-query";
 
-async function fetchData() {
-  const result = await fetch(
-    `https://api.open-meteo.com/v1/forecast?latitude=52.52&longitude=13.41&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`
-  );
+async function fetchData(lati: string, longi: string) {
+  const URL = `https://api.open-meteo.com/v1/forecast?latitude=${lati}&longitude=${longi}&current_weather=true&hourly=temperature_2m,relativehumidity_2m,windspeed_10m`;
+  const result = await fetch(URL);
   const json = await result.json();
   return json;
 }
 
-export function useWeather() {
-  return useQuery(["weather"], fetchData);
+export function useWeather(lati: string, longi: string) {
+  return useQuery(["weather"], () => fetchData(lati, longi);
 }
